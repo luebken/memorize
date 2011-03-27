ff06a40563e691a6302a227c5d08a4b0e0970921
David Aurelio <david@uxebu.com>
2011-03-27 22:26:56 +0200

    Strip-Down for block 2

--- memorize.js
+++ memorize.js
@@ -1,6 +1,4 @@
 var memorize = {
-    containerClassName: "memorize",
-    numPendingInitFuncs: 0,
     init: function(domNode, cols, rows, finishCallback) {
         this.domNode = domNode;
         this.numCols = cols;
@@ -37,48 +35,10 @@ var memorize = {
                 images.unshift("img" + numImages + ".jpg");
             }
         }
-        
+
         // simple sync initialization
-        // this.buildGame();
-
-        // async initialization, give time to register init functions
-        this.isInitialized = true;
-        this.addInitFunc(function(doneCallback){
-            // nothing to do, singalize "ready to go"
-            doneCallback();
-        });
+        this.buildGame();
     },
-
-
-    // ----------------------------------- initialization function registration
-
-    addInitFunc: function(initFunc, args) {
-        this.numPendingInitFuncs += 1;
-
-        args = args || [];
-        // last parameter is a "done" callback that is to be fired when the
-        // init code is done
-        args.push(function() {
-            memorize.initFuncDone();
-        });
-
-        setTimeout(function() {
-            initFunc.apply(null, args);
-        }, 0);
-    },
-
-    initFuncDone: function() {
-        this.numPendingInitFuncs -= 1;
-        if (this.isReady()) {
-            this.buildGame();
-        }
-    },
-
-    isReady: function() {
-        return this.isInitialized && this.numPendingInitFuncs === 0;
-    },
-
-    // ---------------------------------------------------------- game building
 
     buildGame: function() {
         // remove "loading" class
@@ -90,7 +50,6 @@ var memorize = {
         var map = this.createMap(this.numCols, this.numRows);
         var engine = this.createEngine(map);
         this.draw(engine, this._domNode);
-        timer.start();
     },
 
     createMap: function(cols, rows) {
@@ -172,8 +131,8 @@ var memorize = {
             back = doc.createElement("div"),
             front = doc.createElement("div"),
             hint = doc.createElement("span");
-        
-        card["ontouchstart" in document ? "ontouchstart" : "onclick"] = function() {
+
+        card.onclick = function() {
             if(this.className == "card flip") {
                 this.flipToBack();
             } else {
@@ -209,150 +168,10 @@ var memorize = {
     }
 };
 
-var timer = {
-    start : function () {
-        this.startTime = new Date().getTime();
-        this.intervalID = setInterval(this.updateDisplay, 1000);
-    },
-    stop : function () {
-        clearInterval(this.intervalID);
-    },
-    current: function () {
-        var diff = new Date(new Date().getTime() - this.startTime);
-        var secs = diff.getSeconds() + "";
-        if(secs.length < 2) {
-            secs = "0" + secs;
-        }
-        return diff.getMinutes() + ":" + secs;
-    },
-    updateDisplay : function () {
-        document.getElementById('timer').innerHTML = timer.current();
-    }
-}
-
-var helper = {
-    _jsonpId:0,
-    _cb:{},
-    jsonp: function(url, success, error, callbackParamName){
-        /* Simple helper for doing jsonp calls */
-        if(! (url && success)){
-            return;
-        }
-        var script = document.createElement("script"),
-            callbackParamName = callbackParamName ? callbackParamName : "callback",
-            callbackFuncName = '_jsonp'+this._jsonpId++,
-            urlAddon = url.indexOf("?")>-1 ? '&' : '?',
-            urlAddon = urlAddon + callbackParamName + "=" + "helper._cb." + callbackFuncName;
-        this._cb[callbackFuncName] = function(data){
-            success(data);
-        }
-        script.src = url + urlAddon;
-        if(error && typeof(error) == "function"){
-            script.addEventListener("error", error, true);
-        }
-        (document.body || document.documentElement).appendChild(script);
-    }
-}
-
-var numRows = 4;
-var numCols = 4;
 
 window.addEventListener("load", function() {
     var finalize = function () {
-        alert('Congratulations you finished in ' + timer.current());
-        timer.stop();
+        alert('Congratulations!');
     }
-    memorize.init(document.getElementById('playground'), numCols, numRows, finalize);
+    memorize.init(document.getElementById('playground'), 4, 4, finalize);
 }, false);
-
-// Functions for localStorage
-var storage = {
-    supportsLocalStorage: (function(){
-        try {
-            return 'localStorage' in window && window['localStorage'] !== null;
-          } catch(e){
-            return false;
-          }
-    })(),
-    saveImages: function(){
-        if(!this.supportsLocalStorage) return;
-        localStorage.clear();
-        for(var i=0,l=memorize.images.length;i<l;i++){
-            localStorage.setItem("image"+i, memorize.images[i]);
-        }
-    },
-    loadImages: function() {
-        if(!this.supportsLocalStorage) return;
-        for(var i=0,l=localStorage.length;i<l;i++){
-            var key = localStorage.key(i),
-                value = localStorage.getItem(key);
-            if(key.indexOf("image") > -1){
-                memorize.images[key.substring(5)] = value;
-            }
-        }
-    }
-}
-
-// Get geolocation if available
-function getImagesFromFlickrForCity(location, doneCallback) {
-    getImagesFromFlickr('text = "' + location + '" and accuracy = 10', doneCallback);
-}
-
-function getImagesFromFlickrForGeopos(lat, lon, doneCallback) {
-    lat = lat.toFixed(4);
-    lon = lon.toFixed(4);
-    var query = "place_id in (select place.place_id from flickr.places where lat=" + lat + " and lon=" + lon + ") and accuracy = 6";
-    getImagesFromFlickr(query, doneCallback);
-}
-
-function getImagesFromFlickr(query, doneCallback) {
-    var yqlUrl = "http://query.yahooapis.com/v1/public/yql?format=json&q=";
-    query = "select * from flickr.photos.search(" + parseInt(numCols*numRows/2) + ") where " + query + ' and sort = "interestingness-desc"';
-    helper.jsonp(yqlUrl + encodeURIComponent(query), yqlFlickrCallback, function(){
-        // when an error occurs, we gonna try to get the images from our storage
-        storage.loadImages();
-    });
-    yqlFlickrCallback._doneCallback = doneCallback;
-}
-
-function yqlFlickrCallback(json) {
-    var flickrImages = json.query.results.photo; // not failsafe!
-    var images = memorize.images = [];
-    for (var i = 0, image; (image = flickrImages[i]); i++) {
-        flickr_image = "http://farm" + image.farm +
-            ".static.flickr.com/" + image.server + "/" +
-            image.id + "_" + image.secret + "_t.jpg";
-        // append images as data-url (being able to save remote images to localstorage)
-        helper.jsonp(
-            'http://img-to-json.appspot.com/?url='+flickr_image,
-            function(j){ return function(imgData){
-                images[j] = imgData.data;
-                if(memorize.images.length == i){
-                    storage.saveImages();
-                    yqlFlickrCallback._doneCallback();
-                }
-            }}(i)
-        );
-        //images[i] = flickr_image;
-    }
-    //yqlFlickrCallback._doneCallback();
-}
-
-memorize.addInitFunc(function(doneCallback) {
-    function noGeo() {
-        if (console) { console.log("no geo"); }
-        getImagesFromFlickrForCity("M\u00fcnchen", doneCallback);
-    }
-
-    function hasGeo(pos) {
-        if (console) { console.log("has geo"); }
-        var coords = pos.coords;
-        getImagesFromFlickrForGeopos(coords.latitude, coords.longitude, doneCallback);
-    }
-
-    if (navigator.geolocation && navigator.geolocation.getCurrentPosition) {
-        navigator.geolocation.getCurrentPosition(hasGeo, noGeo, {timeout: 10000});
-    } else {
-        noGeo();
-    }
-});
